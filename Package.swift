e792306e553b316f9c993b353c5a6db25ba84f89
bragelbytes <joebragel@gmail.com>
2023-12-14 17:31:11 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/OperatorFoundation/KeychainMacOS.git", branch: "release"),
-        .package(url: "https://github.com/OperatorFoundation/KeychainTypes.git", branch: "release"),
+        .package(url: "https://github.com/OperatorFoundation/KeychainMacOS", branch: "release"),
+        .package(url: "https://github.com/OperatorFoundation/KeychainTypes", branch: "release"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -46,8 +46,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/OperatorFoundation/KeychainLinux.git", branch: "release"),
-        .package(url: "https://github.com/OperatorFoundation/KeychainTypes.git", branch: "release"),
+        .package(url: "https://github.com/OperatorFoundation/KeychainLinux", branch: "release"),
+        .package(url: "https://github.com/OperatorFoundation/KeychainTypes", branch: "release"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
